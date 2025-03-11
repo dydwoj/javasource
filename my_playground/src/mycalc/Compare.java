b9fba11bc158d3da8d12bfac7b33bcc72056ee05
dydwoj <dydwoj@gmail.com>
2025-03-11 18:17:40 +0900

    놀이터

---
+++ my_playground/src/mycalc/Compare.java
@@ -0,0 +1,28 @@
+package mycalc;
+
+public abstract class Compare {
+
+    private int num1;
+    private int num2;
+    private int num3;
+    private int num4;
+    private int num5;
+    private int num6;
+    private int num7;
+    private int num8;
+    private int num9;
+    private int num10;
+
+    public void add() {
+    }
+
+    public void subtract() {
+    }
+
+    public void multiply() {
+    }
+
+    public void divide() {
+    }
+
+}
